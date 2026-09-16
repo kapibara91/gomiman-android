7387adee848a8eb2d4917d58b60c295b2dab5da7
ou <ousannjp@gmail.com>
2026-09-16 23:10:13 +0900

    feat: add app module build configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ configure<ApplicationExtension> {
         applicationId = "co.jp.kpbr.gomiman"
         minSdk = 26
         targetSdk = 37
-        versionCode = 7
-        versionName = "1.4.3"
+        versionCode = 8
+        versionName = "1.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
